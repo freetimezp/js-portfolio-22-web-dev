c6eb14c61ddae25b616df23e6bdf86fc4d445fb9
freetimezp <kraftukrnet@ukr.net>
2023-07-03 22:41:40 +0300

    03. js-portfolio-22-web-dev. Nav Component.

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,3 +1,6 @@
+import { FaHtml5, FaCss3, FaJs, FaReact, FaWordpress, FaFigma } from 'react-icons/fa';
+import { SiNextdotjs, SiFramer, SiAdobexd, SiAdobephotoshop } from 'react-icons/si';
+
 //  data
 const aboutData = [
   {

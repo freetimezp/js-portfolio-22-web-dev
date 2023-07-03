f335ed7f4c32d69812780f741dc9be3b4e084360
freetimezp <kraftukrnet@ukr.net>
2023-07-04 00:22:50 +0300

    05. js-portfolio-22-web-dev. Home Component. ProjectsBtn Component.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,59 @@
+import Image from 'next/image';
+import { motion } from 'framer-motion';
+
+import ParticlesContainer from '../components/ParticlesContainer';
+import ProjectsBtn from '../components/ProjectsBtn';
+import Avatar from '../components/Avatar';
+
+import { fadeIn } from '../variants';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className='bg-primary/60 h-full'>
+      <div className='w-full h-full bg-gradient-to-r from-primary/10 via-black/30 
+      to-black/10'>
+        <div className='text-center flex flex-col justify-center xl:pt-40 xl:text-left 
+        h-full container mx-auto'>
+          <motion.h1
+            variants={fadeIn('down', 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className='h1'
+          >
+            Transforming Ideas <br />
+            Info <span className='text-accent'>Digital Reality</span>
+          </motion.h1>
+
+          <motion.p
+            variants={fadeIn('down', 0.4)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className='max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16'
+          >
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco.
+          </motion.p>
+
+          <div className='flex justify-center xl:hidden relative'>
+            <ProjectsBtn />
+          </div>
+          <motion.div
+            variants={fadeIn('down', 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className='hidden xl:flex'
+          >
+            <ProjectsBtn />
+          </motion.div>
+        </div>
+      </div>
+      <div>image</div>
+    </div>
+  );
 };
 
 export default Home;

7c37cffc086e2397173afb8b270cfca834fa54a4
freetimezp <kraftukrnet@ukr.net>
2023-07-05 23:51:30 +0300

    13. js-portfolio-22-web-dev. Testimonials page. TestimonialSlider Component.

--- pages/work/index.js
+++ pages/work/index.js
@@ -14,7 +14,7 @@ const Work = () => {
             <div className='flex flex-col xl:flex-row gap-x-8'>
                {/* text */}
                <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4
-          xl:mb-0'>
+               xl:mb-0'>
                   <motion.h2
                      variants={fadeIn('up', 0.3)}
                      initial="hidden"
